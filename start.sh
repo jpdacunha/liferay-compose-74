86828fd347a20c85c78fbcbafff6e7757f68bf4b
Jean-Paul Da Cunha <jean-paul.da-cunha@gfi.fr>
2021-06-17 09:08:43 -0700

    Adding Kibana to the stack

---
+++ start.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash
+
+echo " Starting MySQL server"
+sudo docker-compose up -d mysql
+sleep 3s
+echo " Done."
+
+echo " Starting ElasticSearch server"
+sudo docker-compose up -d elasticsearch
+sleep 3s
+echo " Done."
+
+echo " Starting Kibana server"
+sudo docker-compose up -d kibana
+sleep 3s
+echo " Done."
+
+echo " Waiting for readiness of dependent servers ..."
+./utils/spinner.sh sleep 5m
+
+echo " Starting Liferay server"
+sudo docker-compose up -d tomcat
+sudo docker-compose logs --follow tomcat
