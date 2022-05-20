76029558126b9dc4e5a184a663cff8744873e5e4
jpdacunha <dcjeanpaul@gmail.com>
2022-05-20 17:38:06 +0200

    Updating Liferay version

--- start.sh
+++ start.sh
@@ -2,17 +2,17 @@
 #!/bin/bash
 
 echo " Starting MySQL server"
-sudo docker-compose up -d mysql
+sudo docker compose up -d mysql
 sleep 3s
 echo " Done."
 
 echo " Starting ElasticSearch server"
-sudo docker-compose up -d elasticsearch
+sudo docker compose up -d elasticsearch
 sleep 3s
 echo " Done."
 
 echo " Starting Kibana server"
-sudo docker-compose up -d kibana
+sudo docker compose up -d kibana
 sleep 3s
 echo " Done."
 
@@ -20,5 +20,5 @@ echo " Waiting for readiness of dependent servers ..."
 ./utils/spinner.sh sleep 30s
 
 echo " Starting Liferay server"
-sudo docker-compose up -d tomcat
-sudo docker-compose logs --follow tomcat
+sudo docker compose up -d tomcat
+sudo docker compose logs --follow tomcat
