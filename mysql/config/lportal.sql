b4e74207023e41c5799e5dc605b8b6643b11493c
jpdacunha <dcjeanpaul@gmail.com>
2024-07-04 18:58:18 +0200

    Upgrading Liferay Stack

--- mysql/config/lportal.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE DATABASE IF NOT EXISTS lportal74;
-
-ALTER DATABASE lportal74 CHARACTER SET utf8 COLLATE utf8_general_ci;
-
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'password';
-flush privileges;
--- start.sh
+++ start.sh
@@ -1,24 +1,28 @@
 
 #!/bin/bash
 
+echo " Setting host configuration"
+# to avoid elasticsearch to crash
+sudo sysctl -w vm.max_map_count=262144
+
 echo " Starting MySQL server"
-sudo docker compose up -d mysql
+sudo docker compose up -d lc74-mysql
 sleep 3s
 echo " Done."
 
 echo " Starting ElasticSearch server"
-sudo docker compose up -d elasticsearch
+sudo docker compose up -d lc74-elasticsearch
 sleep 3s
 echo " Done."
 
-echo " Starting Kibana server"
-sudo docker compose up -d kibana
-sleep 3s
-echo " Done."
+#echo " Starting Kibana server"
+#sudo docker compose up -d lc74-kibana
+#sleep 3s
+#echo " Done."
 
 echo " Waiting for readiness of dependent servers ..."
 ./utils/spinner.sh sleep 30s
 
 echo " Starting Liferay server"
-sudo docker compose up -d tomcat
-sudo docker compose logs --follow tomcat
+sudo docker compose up -d lc74-liferay
+sudo docker compose logs --follow lc74-liferay
